1f214a8acc676add68348cd61c463824a48ce213
boomikathangaraj <boomikathangaraj@gmail.com>
2026-03-25 20:20:43 +0530

    Added basic C program for academic portfolio

---
+++ basic_c_program.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("Academic Programming Portfolio");
+    printf("\nName: Boomika");
+    printf("\nDepartment: Electronics and Communication Engineering");
+    printf("\nThis program is uploaded as part of my GitHub activity.");
+    
+    return 0;
+}
